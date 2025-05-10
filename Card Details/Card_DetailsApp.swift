e6c217fa7fdb907f555af00ee2971f7a4bf6394a
jakewelliott <jake@jakewelliott.com>
2025-05-09 20:14:38 -0400

    Initial Commit

---
+++ Card Details/Card_DetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_DetailsApp.swift
+//  Card Details
+//
+//  Created by Jacob Elliott on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Card_DetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card DetailsTests/Card_DetailsTests.swift
@@ -0,0 +1,16 @@
+//
+//  Card_DetailsTests.swift
+//  Card DetailsTests
+//
+//  Created by Jacob Elliott on 5/9/25.
+//
+
+import Testing
+
+struct Card_DetailsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
